a773094258320c1f4ba84bb84bb8a92380393883
Marcos Souza Brito <njsu5@hotmail.com>
2022-10-20 19:13:46 -0300

    axios api

--- font-end/src/services/getAllMotors.ts
+++ font-end/src/services/getAllMotors.ts
@@ -1,10 +1,9 @@
 import api from './api';
 
 const getMotors = async () => {
-  const motors = await api.get('motors', {});
-  console.log(motors);
+  const motors = await api.get('motors');
 
-  return motors;
+  return motors.data;
 };
 
 export default getMotors;
